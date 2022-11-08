e74422e4f8db4d58e8e8a26a8ff70b38328b3c27
AkselSundås <akselsundas@gmail.com>
2022-11-08 16:18:30 +0100

    ny text om pushad data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!3333");
+        System.out.println("Hello world!3333. tvingar in ny data från klonen :) ");
     }
 }
